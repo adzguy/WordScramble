80363c128c80883fe2918a6b32367534d3b2694f
adzguy <dnurmeto@neiu.edu>
2019-12-03 03:39:39 -0600

    List with ForEach and Sections.

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -10,7 +10,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        List {
+            Section(header: Text("Section 1")){
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+            }
+
+            Section(header: Text("Section 2")){
+                ForEach(0..<5) {
+                    Text("Dynamuc row \($0)")
+                }
+            }
+
+            Section(header: Text("Section 3")) {
+                Text("Static row 3")
+                Text("Statuc row 4")
+            }
+        }
+    .listStyle(GroupedListStyle())
     }
 }
 
