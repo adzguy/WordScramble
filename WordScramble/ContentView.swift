1d9d4dd333fe2455e0611a1508f903f0b2e82032
adzguy <dnurmeto@neiu.edu>
2019-12-04 13:14:19 -0600

    Before challenge.

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -14,6 +14,10 @@ struct ContentView: View {
     @State private var rootWord = ""
     @State private var newWord = ""
     
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     var body: some View {
     
         NavigationView {
@@ -29,19 +33,85 @@ struct ContentView: View {
                 }
             }
         .navigationBarTitle(rootWord)
+        .onAppear(perform: startGame)
+            .alert(isPresented: $showingError) {
+                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+            }
         }
     }
     
     func addNewWord() {
-        //lowercase and trim the word, to make sure we dont add dublicate words with case differences
+        //lowercase and trim the word, to make sure we don't add dublicate words with case differences
         let word = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         //exit if the remaining string is empty
         guard word.count > 0 else {
             return
         }
+        
+        guard isOriginal(word: word) else {
+            wordError(title: "Word used already", message: "Be more original!")
+            return
+        }
+        guard isPossible(word: word) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        guard isReal(word: word) else {
+            wordError(title: "Word not possible", message: "This isn't a real word")
+            return
+        }
+        
         usedWords.insert(word, at: 0)
         newWord = ""
         
+    }
+    
+    func startGame() {
+        // 1. Find the URL for start.txt in our app bundle
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            // 2. Load start.txt into a string
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                // 3. Split the string up into array of strings, splitting on breaks
+                let allWords = startWords.components(separatedBy: "\n")
+                // 4. Pick one random word, or use "silkworm" as a sensible default
+                rootWord = allWords.randomElement() ?? "silkworm"
+                
+                return
+            }
+        }
+        
+        fatalError("Could not load start.txt from bundle.")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
